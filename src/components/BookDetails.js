b17423542173108d5c49bc43fe7fac6985a1dc55
Nick Gillen <nick.gillen@d51schools.org>
2019-06-20 15:47:56 -0600

    added this.state selected to onClick events in li

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,6 +4,7 @@ import {getBookQuery} from '../queries/queries';
 
 class BookDetails extends Component {
     render(){
+        console.log(this.props);
       return (
         <div id="book-details">
            <p>output book details here</p>
@@ -12,4 +13,12 @@ class BookDetails extends Component {
     }
   }
 
-  export default graphql(getBookQuery)(BookDetails);
+  export default graphql(getBookQuery,{
+      options:(props) => {
+          return{
+              variables: {
+                  id: props.bookId
+              }
+          }
+      }
+  })(BookDetails);
